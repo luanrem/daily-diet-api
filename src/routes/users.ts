f3c2ba235d671427e827e5cec348f93d91041f75
luanrem <luanrem@gmail.com>
2023-05-03 20:37:25 -0300

    feat: create users table migration

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from "fastify";
+import crypto from 'node:crypto'
 import { knex } from "../database";
 
 
@@ -6,7 +7,14 @@ export async function usersRoutes(app: FastifyInstance) {
 
   app.get('/', async (request, reply) => {
     console.log('Its working')
-    const table = await knex('sqlite_schema').select('*')
+    const createNewUser = await knex('users').insert({
+      id: crypto.randomUUID(),
+      name: 'Luan Roberto Estrada Martins',
+      email: 'luanrem@gmail.com'
+    }).returning('*')
+
+    console.log(createNewUser)
+    const table = await knex('users').select('*')
     console.log(table)
 
     reply.send('Hello world')
