233fa99b72fd2fc56ebbd7636243d1ef399d93dd
luanrem <luanrem@gmail.com>
2023-05-03 20:24:47 -0300

    feat: configure database

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance } from "fastify";
+import { knex } from "../database";
 
 
 export async function usersRoutes(app: FastifyInstance) {
 
   app.get('/', async (request, reply) => {
     console.log('Its working')
+    const table = await knex('sqlite_schema').select('*')
+    console.log(table)
 
     reply.send('Hello world')
   })
