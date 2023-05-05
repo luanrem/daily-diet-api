ccc9f065e8eab767d19ff0f1f96da42c5d229a31
luanrem <luanrem@gmail.com>
2023-05-04 22:24:29 -0300

    feat: create meal controller

---
+++ db/migrations/20230505003045_createMeals.ts
@@ -0,0 +1,20 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.timestamp('date_time').notNullable()
+    table.boolean('is_on_diet').defaultTo(false).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.uuid('user_id').references('id').inTable('users')
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
+
--- src/routes/meal.ts
+++ src/routes/meal.ts
@@ -1,5 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
+import { knex } from "../database";
+import { z } from "zod";
+import { randomUUID } from "crypto";
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.addHook('preHandler', async (request, reply) => {
@@ -11,8 +14,32 @@ export async function mealsRoutes(app: FastifyInstance) {
       preHandler: [checkSessionIdExists],
     },
     async (request, reply) => {
+      const createNewMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        date_time: z.string(),
+        is_on_diet: z.boolean()
+      })
+
+      const { name, description, date_time, is_on_diet } = createNewMealBodySchema.parse(request.body)
+
       const { sessionId } = request.cookies
 
-      console.log('created', sessionId)
+      const user = await knex('users')
+        .where('session_id', sessionId)
+        .select()
+
+      // return { user }
+
+      const meal = await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        description,
+        date_time: new Date(date_time),
+        is_on_diet,
+        user_id: user[0].id
+      }).returning('*')
+
+      return { meal }
     })
 }
