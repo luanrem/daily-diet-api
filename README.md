2c8452122c10718fdd71aa737bb06a09558bd326
luanrem <luanrem@gmail.com>
2023-05-06 11:54:29 -0300

    feat: edit meal

--- README.md
+++ README.md
@@ -61,7 +61,7 @@ So I will make as checkbox what need to be implemented and what I've already don
     - Date and Time
     - It's on the diet or not?
 
-- [ ] It should be possible to edit a meal, being able to change all the above data
+- [x] It should be possible to edit a meal, being able to change all the above data
 - [x] It should be possible to delete a meal
 - [x] It should be possible to list all meals of a user
 - [x] It should be possible to visualize a single meal
--- src/routes/meal.ts
+++ src/routes/meal.ts
@@ -103,4 +103,50 @@ export async function mealsRoutes(app: FastifyInstance) {
         }).delete()
 
     })
+
+  app.patch('/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const createNewMealBodySchema = z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        date_time: z.string().optional(),
+        is_on_diet: z.boolean().optional()
+      })
+      const createNewMealParamSchema = z.object({
+        id: z.string().uuid()
+      })
+
+      const { id } = createNewMealParamSchema.parse(request.params)
+      const { date_time, description, is_on_diet, name } = createNewMealBodySchema.parse(request.body)
+
+      const { sessionId } = request.cookies
+
+      const user = await knex('users')
+        .where('session_id', sessionId)
+        .select()
+
+      const isThisMealFromUser = await knex('meals').where({
+        user_id: user[0].id,
+        id
+      })
+
+      if (!isThisMealFromUser) {
+        return reply.status(404).send('This meal is not related to this user')
+      }
+
+      const newDate = date_time ? format(new Date(date_time), "yyyy'-'MM'-'dd' 'HH:mm:ss") : undefined
+
+      console.log(user)
+      const meal = await knex('meals').where({ id }).update({
+        name,
+        date_time: newDate,
+        description,
+        is_on_diet
+      })
+
+      console.log(meal)
+    })
 }
